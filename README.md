e55239a0104cde29cac447ef994944282af3c1d0
Sadeesha Perera <121984253+SadeeshaPerera@users.noreply.github.com>
2024-04-09 02:14:11 +0530

    Added Micro Frontend Architecture Successfully

--- README.md
+++
@@ -1,2 +0,0 @@
-# Blissify--Wedding-Management-System
-SLIIT 2nd year second semester ITP module project using MERN stack technology for a wedding management system.
--- main-app/frontend/src/remote/remoteTypes.d.ts
+++
@@ -1,9 +0,0 @@
-type Props = {};
-declare module "VendorModule/Vendor" {
-  const Vendor: React.ComponentType<Props>;
-  export default Vendor;
-}
-declare module "AnotherModule/SampleComponent" {
-  const SampleComponent: React.ComponentType<Props>;
-  export default SampleComponent;
-}
