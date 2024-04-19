c02d8b8f6b5bfba357ddf59bd2c5bee77a79ff7d
JanudiAdhikari <januditharushika@gmail.com>
2024-04-19 22:34:14 +0530

    Added Budget Management Codes

---
+++ budgetapp/frontend/src/pages/budgetManagement/expenses/ExpenseForm.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
+import Button from '../../../components/Button/Button'
+import { plus } from '../../../assets/budgetImages/budgetIcons'
+import './expenseform.css'
+import axios from 'axios'
+
+
+const ExpenseForm = () => {
+
+    const [expenses, setExpenses] = useState([])
+    const [error, setError] = useState(null)
+
+    const addExpense = async (expense) => {
+        const response = await axios.post("http://localhost:8000/api/addExpense", expense)
+            .catch((err) =>{
+                setError(err.response.data.message)
+            })
+        getExpenses()
+    }
+
+    const getExpenses = async () => {
+        const response = await axios.get("http://localhost:8000/api/getExpenses")
+        setExpenses(response.data)
+        console.log(response.data)
+    }
+
+    const [inputState, setInputState] = useState({
+        title: '',
+        amount: '',
+        date: '',
+        category: '',
+        description: '',
+    })
+
+    const { title, amount, date, category,description } = inputState;
+
+    const handleInput = name => e => {
+        setInputState({...inputState, [name]: e.target.value})
+        setError('')
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        addExpense(inputState)
+        setInputState({
+            title: '',
+            amount: '',
+            date: '',
+            category: '',
+            description: '',
+        })
+    }
+
+    return (
+        <div className='ExpenseFormStyled' onSubmit={handleSubmit}>
+            {error && <p className='error'>{error}</p>}
+            <div className="input-control">
+                <input 
+                    type="text" 
+                    value={title}
+                    name={'title'} 
+                    placeholder="Expense Title"
+                    onChange={handleInput('title')}
+                />
+            </div>
+            <div className="input-control">
+                <input value={amount}  
+                    type="text" 
+                    name={'amount'} 
+                    placeholder={'Expense Amount'}
+                    onChange={handleInput('amount')} 
+                />
+            </div>
+            <div className="input-control">
+                <DatePicker 
+                    id='date'
+                    placeholderText='Enter A Date'
+                    selected={date}
+                    dateFormat="dd/MM/yyyy"
+                    onChange={(date) => {
+                        setInputState({...inputState, date: date})
+                    }}
+                />
+            </div>
+            <div className="selects input-control">
+                <select required value={category} name="category" id="category" onChange={handleInput('category')}>
+                    <option value="" disabled >Select Option</option>
+                    <option value="education">Education</option>
+                    <option value="groceries">Groceries</option>
+                    <option value="health">Health</option>
+                    <option value="subscriptions">Subscriptions</option>
+                    <option value="takeaways">Takeaways</option>
+                    <option value="clothing">Clothing</option>  
+                    <option value="traveling">Traveling</option>  
+                    <option value="other">Other</option>  
+                </select>
+            </div>
+            <div className="input-control">
+                <textarea name="description" value={description} placeholder='Add A Reference' id="description" cols="30" rows="4" onChange={handleInput('description')}></textarea>
+            </div>
+            <div className="submit-btn">
+                <Button 
+                    name={'Add Expense'}
+                    icon={plus}
+                    bPad={'.8rem 1.6rem'}
+                    bRad={'30px'}
+                    bg={'#4d1c9c'}
+                    color={'#fff'}
+                    onClick={handleSubmit}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseForm
