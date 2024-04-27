ef02a290030663a54db77276d2ab17f09a3d4e48
JanudiAdhikari <januditharushika@gmail.com>
2024-04-28 04:54:44 +0530

    Edited All Transactions Page

--- budgetapp/frontend/src/pages/budgetManagement/allTransactions/AllTransactions.jsx
+++ budgetapp/frontend/src/pages/budgetManagement/allTransactions/AllTransactions.jsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -7,6 +8,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
+import moment from "moment";
 
 const columns = [
   { id: "title", label: "Title", minWidth: 100 },
@@ -21,8 +23,7 @@ const columns = [
     id: "amount",
     label: "Amount\u00a0(Rs.)",
     minWidth: 100,
-    align: "center",
-    format: (value) => value.toLocaleString("en-US"),
+    align: "right"
   },
   {
     id: "date",
@@ -38,30 +39,31 @@ const columns = [
   },
 ];
 
-function createData(title, type, category, amount) {
-  const date = category / amount;
-  return { title, type, category, amount, date };
-}
+const dateFormat = (date) => {
+  return moment(date).format("DD/MM/YYYY");
+};
 
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-];
 
-export default function StickyHeadTable() {
+export default function AllTransactions() {
+  const [rowsNew, setRowsNew] = useState([]);
+  const [exp, setExp] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const incomeResponse = await axios.get("http://localhost:8000/api/getIncomes");
+      const expenseResponse = await axios.get("http://localhost:8000/api/getExpenses");
+
+      const combinedData = [...incomeResponse.data, ...expenseResponse.data];
+      // Sort the combined data array based on date in descending order
+      combinedData.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+      setRowsNew(combinedData);
+      setExp(expenseResponse.data);
+    };
+
+    fetchData();
+  }, []);
+
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
@@ -92,31 +94,31 @@ export default function StickyHeadTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows
+            {rowsNew
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.type}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === "number"
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
+              .map((row, index) => (
+                <TableRow hover role="checkbox" tabIndex={-1} key={index}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+      {column.id === 'date' ? dateFormat(value) : (
+        column.format && typeof value === "number"
+          ? column.format(value)
+          : value
+      )}
+    </TableCell>
+                    );
+                  })}
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
       <TablePagination
         rowsPerPageOptions={[10, 25, 100]}
         component="div"
-        count={rows.length}
+        count={rowsNew.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
@@ -125,3 +127,4 @@ export default function StickyHeadTable() {
     </Paper>
   );
 }
+
